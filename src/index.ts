be8203cc653a6364e5adb901d06196ac42294d43
andresf0090@gmail.com <andresf0090@gmail.com>
2025-06-08 03:05:22 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+export const app = express()
+const PORT = process.env.PORT
+
+app.listen(PORT, () =>{
+    console.log('app is working')
+})
+
